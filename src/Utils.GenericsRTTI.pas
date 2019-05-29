bae674cd11e68db39bf7e46b9d60786b985cb40b
Victor Zanella <victor.t.y.z@hotmail.com>
2019-05-29 00:47:26 -0300

    New features

--- src/Utils.GenericsRTTI.pas
+++ src/Utils.GenericsRTTI.pas
@@ -3,19 +3,24 @@ unit Utils.GenericsRTTI;
 interface
 
 uses
-  System.Generics.Collections;
+  System.Generics.Collections,
+  RTTI;
 
 type
   TUtilsGenericsRTTI<T: class, constructor> = class(TEnumerable<T>)
     class procedure Clone(AFromList, AToList: TObjectList<T>);
+    class function CreateObjectByClassType: T;
+  end;
+
+  TUtilsRTTI = class
+    class function CreateObjectByClassName(AClassName: String): TValue;
   end;
 
 implementation
 
-uses
-  Rtti;
-
 { TUtilsGenericsRTTI<T> }
+
+
 
 class procedure TUtilsGenericsRTTI<T>.Clone(AFromList, AToList: TObjectList<T>);
 var
@@ -32,6 +37,31 @@ begin
     end;
 
   end;
+
+end;
+
+
+
+class function TUtilsGenericsRTTI<T>.CreateObjectByClassType: T;
+var
+  Tipo: TRttiType;
+begin
+  Tipo := TRttiContext.Create.GetType(T.ClassInfo);
+  if Tipo <> nil then
+    Result := Tipo.AsInstance.GetMethod('Create').Invoke(Tipo.AsInstance.MetaclassType, [nil]).AsType<T>;
+end;
+
+{ TUtilsRTTI }
+
+
+
+class function TUtilsRTTI.CreateObjectByClassName(AClassName: String): TValue;
+var
+  Tipo: TRttiType;
+begin
+  Tipo := TRttiContext.Create.FindType(AClassName);
+  if Tipo <> nil then
+    Result := Tipo.AsInstance.GetMethod('Create').Invoke(Tipo.AsInstance.MetaclassType, [nil]);
 
 end;
 
